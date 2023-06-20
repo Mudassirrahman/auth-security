305b0f39e21b0400661a22f4590ad38f6a75fad4
mudassirrahman <mudassirrehman1989@gmail.com>
2023-06-20 10:10:58 +0500

    access token with jwt auth guard implementeation done.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,40 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SigninDto } from './dtos/signin.dto';
 import { UserEntity } from 'src/user/entities/user.entity';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { AuthGuard, Public } from 'src/guards/auth.guard';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
-    @HttpCode(HttpStatus.OK)
-    @Post('login')
-    signIn(@Body() signinDto: SigninDto): Promise<Partial<UserEntity>> {
-      return this.authService.signIn(signinDto);
-    }
-    @HttpCode(HttpStatus.CREATED)
+  @Public()
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signinDto: SigninDto): Promise<Partial<UserEntity>> {
+    return this.authService.signIn(signinDto);
+  }
+
+  @HttpCode(HttpStatus.CREATED)
   @Post('register')
   register(@Body() CreateUserDto: CreateUserDto): Promise<Partial<UserEntity>> {
     return this.authService.register(CreateUserDto);
+  }
+
+  // @UseGuards(AuthGuard)
+  // @Public()
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,21 +3,30 @@ import { UserService } from 'src/user/user.service';
 import { SigninDto } from './dtos/signin.dto';
 import { UserEntity } from 'src/user/entities/user.entity';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UserService) {}
+  constructor(private userService: UserService ,
+    private readonly jwtService: JwtService) {}
 
-    async signIn(signinDto: SigninDto): Promise<Partial<UserEntity>> {
-      const {username, password: pass} = signinDto
-      const user = await this.userService.findOne(username);
-      if (user?.password !== pass) {
-        throw new UnauthorizedException();
-      }
-      const { password, ...result } = user;
-    return result;
+  async signIn(signinDto: SigninDto): Promise<any> {
+    const { username, password: pass } = signinDto;
+    const user = await this.userService.findOne(username);
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const { password, ...result } = user;
+
+    const payload = { id: user.id, username: user.firstname };
+    const token = await this.jwtService.signAsync(payload, {
+      secret: "hello",
+      expiresIn: "5m",
+    })
+    ;
+    return token;
   }
-   
+
   async register(createUserDto: CreateUserDto): Promise<Partial<UserEntity>> {
     const user = await this.userService.createUser(createUserDto);
     const { password, ...result } = user;
